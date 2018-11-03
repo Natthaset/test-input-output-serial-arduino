0b5f7315e8fa899a1d7fce7e09dd624292dcc24d
Natthaset <natthaset.palm@gmail.com>
2018-11-03 21:05:11 +0700

    first commit

---
+++ code.c
@@ -0,0 +1,29 @@
+String st;
+int num;
+
+void setup() {
+
+Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+}
+
+void loop() {
+
+while(Serial.available()) { //เปิดใช้งานการรับค่าจาก Serial
+
+st = Serial.readString();// read the incoming data as string
+
+  if(st.substring(0,1) == "a"){
+    String stToNum = st.substring(1);
+    num = stToNum.toInt();
+    Serial.println("Stirng : "+st.substring(0,1));
+    Serial.print("Number :");
+    Serial.println(num);
+  }else{
+    Serial.println("Not read");
+  }
+
+
+}
+
+}
