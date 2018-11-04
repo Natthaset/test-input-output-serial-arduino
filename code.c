571ad8daf8ce28b77ef3fe6b2a04e7c751b415d8
Natthaset <natthaset.palm@gmail.com>
2018-11-04 12:17:29 +0700

    up-to-date

--- code.c
+++ code.c
@@ -10,20 +10,24 @@ Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 void loop() {
 
 while(Serial.available()) { //เปิดใช้งานการรับค่าจาก Serial
-
-st = Serial.readString();// read the incoming data as string
-
+  st = Serial.readString();// read the incoming data as string
   if(st.substring(0,1) == "a"){
     String stToNum = st.substring(1);
     num = stToNum.toInt();
     Serial.println("Stirng : "+st.substring(0,1));
     Serial.print("Number :");
     Serial.println(num);
+    Serial.print("Send paramiter to func int : ");
+    Serial.println(sum(num));
   }else{
     Serial.println("Not read");
   }
-
+}
 
 }
 
+int sum(int number){
+  int result;
+  result = number * number;
+  return result;
 }
